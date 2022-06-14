9a59e1847700e72c868042d2de89c263f653d8ae
tgallagher496 <tgallagher496@gmail.com>
2022-06-13 22:14:18 -0400

    added fetch

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var searchFormEl = document.querySelector('#search-form');
+var APIKey = "45be21ec4c6acb1dabb011fa26f9fbd1";
+var searchFormEl = document.getElementById('search-form');
+var cityEl = document.getElementById('City');
 
 function getInput(){
 
@@ -11,9 +13,26 @@ function getInput(){
         return;
     }
 
-    //var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-    console.log(userInput);
+    var city = document.getElementById("search-input").value;
+    console.log(city);
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+
+    fetch(queryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        console.log(data.main.temp)
+        document.getElementById('City').innerHTML = data.name;
+
+    });
+
+
 }
 
 searchFormEl.addEventListener('submit', getInput);
+
+
+
 
